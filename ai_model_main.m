0134d60e6eb619805e4b8c0263d9058ecf2f07a6
tamaraceranic <132878959+tamaraceranic@users.noreply.github.com>
2026-06-11 15:13:07 +0200

    Update ai_model_main.m

--- ai_model_main.m
+++ ai_model_main.m
@@ -1,6 +1,7 @@
 function [predictied_label]=ai_model_main(TRAINING,label_training,TEST)
    
-
+% The model parameters are set to values that achieved strong performance on the available test datasets. 
+% Users are encouraged to further experiment with and optimize hyperparameters on their own datasets, as performance may vary depending on data characteristics.
 %% AdaBoostM1 for depth 3, and 'Tree' 300. 
 
          treeTemplate = templateTree('MaxNumSplits', 3); % ? depth 2
